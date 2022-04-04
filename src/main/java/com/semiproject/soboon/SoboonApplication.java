35bc10d1a4371e48dd3c788e0c5efef8246f0154
hyeon3 <dldlsrn231@gmail.com>
2022-04-04 16:55:48 +0900

    update

---
+++ src/main/java/com/semiproject/soboon/SoboonApplication.java
@@ -0,0 +1,13 @@
+package com.semiproject.soboon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoboonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoboonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semiproject/soboon/SoboonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semiproject.soboon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoboonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
