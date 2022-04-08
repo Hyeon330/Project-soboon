62bfd4731eb3f83a600e41eec59bbf1190a56d81
hyeon3 <dldlsrn231@gmail.com>
2022-04-08 15:21:45 +0900

    update

---
+++ src/main/java/com/semiproject/soboon/vo/MemberVO.java
@@ -0,0 +1,89 @@
+package com.semiproject.soboon.vo;
+
+public class MemberVO {
+	private String userid;
+	private String userpwd;
+	private String username;
+	
+	private String tel;
+	private String tel1;
+	private String tel2;
+	private String tel3;
+	
+	private String nickname;
+	private String email;
+	private String address;
+	private int warn;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getTel() {
+		tel = tel1+"-"+tel2+"-"+tel3;
+		return tel;
+	}
+	public void setTel(String tel) {
+		String tels[] = tel.split("-");
+		tel1 = tels[0];
+		tel2 = tels[1];
+		tel3 = tels[2];
+		this.tel = tel;
+	}
+	public String getTel1() {
+		return tel1;
+	}
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	public String getTel2() {
+		return tel2;
+	}
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+	public String getTel3() {
+		return tel3;
+	}
+	public void setTel3(String tel3) {
+		this.tel3 = tel3;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getWarn() {
+		return warn;
+	}
+	public void setWarn(int warn) {
+		this.warn = warn;
+	}
+}
---
+++ src/main/webapp/js/member.js
@@ -0,0 +1,82 @@
+function memberCheck(){
+	var reg = /^[a-zA-Z]{1}[a-zA-Z0-9_]{5,11}$/;
+	if($("#userid").val()==""){
+		alert("아이디를 입력하세요.");
+		$("#userid").focus();
+		return false;
+	}
+	if(!reg.test($("#userid").val())){
+		alert("아이디를 잘못 입력하였습니다.\n아이디는 6~12 사이여야 합니다.\n아이디는 영문자로 시작해야합니다.");
+		$("#userid").focus();
+		return false;
+	}
+	
+	reg = /^[a-zA-Z0-9~!@#^*_]{7,15}$/;
+	if($("#userpwd").val()==''){
+		alert("비밀번호를 입력하세요.");
+		$("#userpwd").focus();
+		return false;
+	}
+	if (!reg.test($("#userpwd").val())) {
+		alert("비밀번호를 잘못 입력하였습니다.\n비밀번호는 8~20자의 영문 소대문자, 숫자와 특수기호 '~!@#^*_'만 사용 가능합니다.");
+		$("#userpwd").focus();
+		return false;
+	}
+	if ($("#userpwd").val() != $("#userpwd2").val()) {
+		alert("비밀번호가 일치하지 않습니다.\n다시 입력해주세요.");
+		$("#userpwd2").focus();
+		return false;
+	}
+	
+	reg = /^[가-힣]{2,7}$/;
+	if($("#username").val()==''){
+		alert("이름을 입력하세요.");
+		$("#username").focus();
+		return false;
+	}
+	if (!reg.test($("#username").val())) {
+		alert("이름을 잘못 입력하였습니다. 이름은 2~7글자내로 입력하셔야 합니다.");
+		$("#username").focus();
+		return false;
+	}  
+	
+	reg = /^[가-힣]{2,7}$/;
+	if($("#nickname").val()==''){
+		alert("닉네임을 입력하세요.");
+		$("#nickname").focus();
+		return false;
+	}
+	if (!reg.test($("#nickname").val())) {
+		alert("닉네임을 잘못 입력하였습니다. 닉네임은 2~7글자내로 입력하셔야 합니다. 한글만 가능합니다.");
+		$("#nickname").focus();
+		return false;
+	}  
+	
+	var tel = $("#tel1").val() + "-";
+		tel += $("#tel2").val() + "-";
+		tel += $("#tel3").val();
+	reg = /^(010|02|031|032|041|043|051|061)[-][0-9]{3,4}[-][0-9]{4}$/;
+	if (!reg.test(tel)) {
+		alert("전화번호를 잘못 입력하였습니다.");
+		$("#tel1").focus();
+		return false;
+	}
+	
+	reg=/([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;	
+	if($("#email").val()==''){
+		alert('이메일을 입력하세요.');
+		$("#email").focus();
+		return false;
+	}
+	if (!reg.test($("#email").val())) {
+		alert("이메일을 잘못 입력하였습니다. \n예시)example@naver.com");
+		$("#email").focus();
+		return false;
+	}
+	
+	if($("#address").val()==''){
+		alert("주소를 입력하세요.");
+		$("#address").focus();
+		return false;
+	}
+}
