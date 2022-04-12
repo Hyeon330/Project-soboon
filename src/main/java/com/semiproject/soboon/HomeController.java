9fc3160522f797ea7ab5a20288004ce4802624fd
SeongSilver <yooseongeun@naver.com>
2022-04-12 14:22:53 +0900

    성은 home 작업 끝

--- src/main/java/com/semiproject/soboon/HomeController.java
+++ src/main/java/com/semiproject/soboon/HomeController.java
@@ -1,15 +1,24 @@
 package com.semiproject.soboon;
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.semiproject.soboon.service.HomeService;
+
 @Controller
 public class HomeController {
+	@Inject
+	HomeService service;
+	
 	@GetMapping("/")
-	public String home(HttpSession session) {
-		return "home";
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("list", service.selectList());
+		mav.setViewName("home");
+		return mav;
 	}
 	
 	@GetMapping("/areaSelect")
