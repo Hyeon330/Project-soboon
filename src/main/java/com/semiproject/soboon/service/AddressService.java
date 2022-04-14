4fcfd532257c3044d17a83c77b44ae47e9ab0717
hyeon3 <dldlsrn231@gmail.com>
2022-04-14 14:53:07 +0900

    update

--- src/main/java/com/semiproject/soboon/service/AddressService.java
+++ src/main/java/com/semiproject/soboon/service/AddressService.java
@@ -3,7 +3,6 @@ package com.semiproject.soboon.service;
 import java.util.List;
 
 public interface AddressService {
-
 	public List<String> getLargeAddr();
 	public List<String> getMediumAddr(String large);
 	public List<String> getSmallAddr(String large, String medium);
