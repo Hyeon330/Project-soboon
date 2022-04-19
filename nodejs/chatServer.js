5328d37d7021e80fbfc9ce98d0c2b5206e1962a3
hyeon3 <dldlsrn231@gmail.com>
2022-04-19 10:16:57 +0900

    채팅 완료!!

--- nodejs/chatServer.js
+++ nodejs/chatServer.js
@@ -14,27 +14,60 @@ conn.connect();
 
 var userList = [];
 io.sockets.on('connection', (socket) => {
-    socket.on('send-msg', (data) => {
-        console.log(data.chat_read);
+    socket.on('send-msg', async (data) => {
         if(data.chat_read=='end'){
             var insertMsgSql = 'insert into chat(sender, receiver, msg, chat_read) values (?, ?, ?, ?)';
             conn.execute(insertMsgSql, [data.sender, data.receiver, data.msg, data.chat_read], () => {});
+
+            var endCheckSql = 'select chat_read from chat ';
+            endCheckSql += 'where (sender=? and receiver=?) ';
+            endCheckSql += 'or (receiver=? and sender=?) ';
+            endCheckSql += 'order by chatno desc limit 2';
+            await conn.execute(endCheckSql, [data.sender, data.receiver, data.sender, data.receiver], async (err, result) => {
+                let cnt = 0;
+                result.forEach(d => {
+                    if(d.chat_read=='end') cnt++;
+                });
+                if(cnt >= 2){
+                    // 삭제되는 메시지 chat_remove테이블로 옮겨 담기
+                    var selectRemoveChatsSql = 'select * from chat ';
+                    selectRemoveChatsSql += 'where (sender=? and receiver=?) ';
+                    selectRemoveChatsSql += 'or (receiver=? and sender=?) ';
+                    conn.execute(selectRemoveChatsSql, [data.sender, data.receiver, data.sender, data.receiver], (e2, r2) => {
+                        var insertRemoveChatsSql = 'insert into chat_remove (chatno, sender, receiver, msg, chat_datetime, chat_read) ';
+                        insertRemoveChatsSql += 'values (?,?,?,?,?,?)';
+                        r2.forEach(d => {
+                            conn.execute(insertRemoveChatsSql, [d.chatno, d.sender, d.receiver, d.msg, d.chat_datetime, d.chat_read], () => {})
+                        });
+                    });
+
+                    // 메시지 삭제
+                    var deleteChatsSql = 'delete from chat ';
+                    deleteChatsSql += 'where (sender=? and receiver=?) ';
+                    deleteChatsSql += 'or (receiver=? and sender=?) ';
+                    await conn.execute(deleteChatsSql, [data.sender, data.receiver, data.sender, data.receiver], () => {});
+                }
+            });
         }else {
             var insertMsgSql = 'insert into chat(sender, receiver, msg) values (?, ?, ?)';
             conn.execute(insertMsgSql, [data.sender, data.receiver, data.msg], () => {});
         }
         
+        // var selectMsgSql = 'select * from chat ';
+        // selectMsgSql += 'where chatno=( ';
+        // selectMsgSql += 'select max(chatno) from chat ';
+        // selectMsgSql += 'where (sender=? and receiver=?))';
         var selectMsgSql = 'select * from chat ';
-        selectMsgSql += 'where chatno=( ';
-        selectMsgSql += 'select max(chatno) from chat ';
-        selectMsgSql += 'where (sender=? and receiver=?))';
-        conn.execute(selectMsgSql, [data.sender, data.receiver], (err, result) => {
-            var chatDateTimeArr = String(result[0].chat_datetime).split(' ');
-            result[0].chat_datetime = chatDateTimeArr[3]+'-0'+(result[0].chat_datetime.getMonth()+1)+'-'+result[0].chat_datetime.getDate()+' '+chatDateTimeArr[4];
-            io.emit('receive-msg', result[0]);
+        selectMsgSql += 'where sender=? and receiver=? ';
+        selectMsgSql += 'order by chatno desc limit 1';
+        await conn.execute(selectMsgSql, [data.sender, data.receiver], (err, result) => {
+            if(result.chat_read != 'end'){
+                var chatDateTimeArr = String(result[0].chat_datetime).split(' ');
+                result[0].chat_datetime = chatDateTimeArr[3]+'-0'+(result[0].chat_datetime.getMonth()+1)+'-'+result[0].chat_datetime.getDate()+' '+chatDateTimeArr[4];
+                io.emit('receive-msg', result[0]);
+            }
         });
     });
-
 });
 
 server.listen(9001, () => {
