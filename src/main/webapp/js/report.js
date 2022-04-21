f392bc47ac19a7dae90ba57cd54d76dc84b331ed
hdorm518 <wnrehqhd1@naver.com>
2022-04-21 11:29:26 +0900

    update

--- src/main/webapp/js/report.js
+++ src/main/webapp/js/report.js
@@ -1,14 +1,24 @@
 $(function() {
-	$('#proWarn').click(function() {
+	$('#proWarn').click(function(){
 		$('#reportReason').dialog('open');
 	});
 	$('#reportReason').dialog({
 		title: "신고사유",
 		autoOpen: false,
+		modal: true,
 		buttons: {
 			등록: function() {
-				alert('등록');
-				$("#reportSubmit").submit();
+				$.ajax({
+					url:"/report/insertReportContent",
+					data:"report_content="+$("#reportContent").val() + "&suspect="+$("#viewNickname").text() + "&board_no="+$("#reportReason").attr("no"),
+					type:"POST",
+					success:function(result){
+						if(result>0){
+							alert("신고가 접수되었습니다.");
+						}
+					}
+				});
+				$("#reportReason").dialog('close');
 			},
 			취소: function() {
 				$("#reportReason").dialog('close');
